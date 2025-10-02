76f56d4f99e134763881b40f3bfb572279adb66e
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2025-10-02 18:23:40 -0400

    CONFIGURATION UPDATE! YAY!

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Anti-Grian Switch reborn
 
-This is a reproduction of the Anti-Grian switch from Hermitcraft season 9, and Anti-Gem switch from Hermitcraft season 10. It allows you to toggle an error that existed in the Continuity mod which did not play nice with sodium rendercode. But this can be used anywhere, that's right, anywhere! With (I hope) any mod. Right now, you toggle it using the ` key, I will probably add a config screen later.
+This is a reproduction of the Anti-Grian switch from Hermitcraft season 9, and Anti-Gem switch from Hermitcraft season 10. It allows you to toggle an error that existed in the Continuity mod which did not play nice with sodium rendercode. But this can be used anywhere, that's right, anywhere! With (I hope) any mod.
+
+## Use
+
+By default, you configure the anti Grian switch using `CTRL + G`. You use the keybind `Should Control Be Held (AGS Reborn)(y/n)` to, as the name implies, configure wether or not the control key should be held. If it's set to `n`, than you don't need to hold control to activate it, if it's set to anything else, it will auto-set to `y`. If you place a gravity block when the switch is set to on, then the game will crash.
+
+## How it works
+
+This mod takes advantage of a strange quirk in how minecraft renders. This is based off a cross-compatibility error between [Continuity](https://modrinth.com/mod/continuity) and [Sodium](https://modrinth.com/mod/sodium) in 1.19 versions when [Indium](https://modrinth.com/mod/indium) where Sodium's rendering engine did not handle Fabric Rendering API calls correctly, and threw nullptr errors. My mod uses this code:
+
+```Java
+if(AntiGrianSwitchReborn.enableFallingEntityBug) {
+    Object renderer = null;
+    renderer.toString();
+}
+```
+
+to crash the game when it tries to render a falling block entity. It is a renderthread crash, which is considered a "safe" crash. It should not corrupt your worlds¹ at least, it didn't in my testing.
+
+---
+_¹I DO NOT take **any** responsibility for corrupted worlds, server bans, or lost data caused by ~~abusing~~ leveraging this mod's core function_
--- src/main/java/dev/gamelord2011/ags_reborn/AntiGrianSwitchReborn.java
+++ src/main/java/dev/gamelord2011/ags_reborn/AntiGrianSwitchReborn.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 //import net.minecraft.client.util.InputUtil;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import org.lwjgl.glfw.GLFW;
@@ -14,6 +15,7 @@ public class AntiGrianSwitchReborn implements ClientModInitializer {
 	public static final String MOD_ID = "anti-grian-switch-reborn";
     public static boolean enableFallingEntityBug = false;
     private static KeyBinding TGG;
+    private static KeyBinding CCB;
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -24,18 +26,38 @@ public class AntiGrianSwitchReborn implements ClientModInitializer {
     public void onInitializeClient() {
         TGG = KeyBindingHelper.registerKeyBinding(new KeyBinding(
             "Anti Grian Switch", // The translation key of the keybinding's name
-            GLFW.GLFW_KEY_GRAVE_ACCENT, // The keycode of the key
+            GLFW.GLFW_KEY_G, // The keycode of the key
+            KeyBinding.Category.GAMEPLAY
+        ));
+
+        CCB = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "Should Control Be Held (AGS Reborn)(y/n)", // The translation key of the keybinding's name
+            GLFW.GLFW_KEY_Y,
             KeyBinding.Category.GAMEPLAY
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (TGG.wasPressed()) {
-                enableFallingEntityBug = !enableFallingEntityBug;
-                MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(
-                    net.minecraft.text.Text.literal(
-                        "Anti Grian Switch: " + (AntiGrianSwitchReborn.enableFallingEntityBug ? "ON" : "OFF")
-                    )
-                );
+            if(KeyBindingHelper.getBoundKeyOf(CCB).getCode() != GLFW.GLFW_KEY_N) {
+                while (TGG.wasPressed() && ((GLFW.glfwGetKey(client.getWindow().getHandle(), GLFW.GLFW_KEY_LEFT_CONTROL) == GLFW.GLFW_PRESS) || (GLFW.glfwGetKey(client.getWindow().getHandle(), GLFW.GLFW_KEY_RIGHT_CONTROL) == GLFW.GLFW_PRESS))) {
+                    enableFallingEntityBug = !enableFallingEntityBug;
+                    MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(
+                        net.minecraft.text.Text.literal(
+                            "Anti Grian Switch: " + (AntiGrianSwitchReborn.enableFallingEntityBug ? "ON" : "OFF")
+                        )
+                    );
+                }
+            } else {
+                if(KeyBindingHelper.getBoundKeyOf(CCB).getCode() != GLFW.GLFW_KEY_Y) {
+                    CCB.setBoundKey(InputUtil.Type.KEYSYM.createFromCode(GLFW.GLFW_KEY_Y));
+                }
+                while (TGG.wasPressed()) {
+                    enableFallingEntityBug = !enableFallingEntityBug;
+                    MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(
+                        net.minecraft.text.Text.literal(
+                            "Anti Grian Switch: " + (AntiGrianSwitchReborn.enableFallingEntityBug ? "ON" : "OFF")
+                        )
+                    );
+                }
             }
         });
     }
