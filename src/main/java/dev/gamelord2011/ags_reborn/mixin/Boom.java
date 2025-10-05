b75592d90bb87bb0590e2a244044ffe131e069d6
strubium <113206902+strubium@users.noreply.github.com>
2025-10-05 13:06:55 -0500

    Revert "Basic Forge 1.12.2 Port (needs lang)"
    This reverts commit ac013a4b16384beaa49ed4d1a5d6aca204a5a8f3.

---
+++ src/main/java/dev/gamelord2011/ags_reborn/mixin/Boom.java
@@ -0,0 +1,36 @@
+package dev.gamelord2011.ags_reborn.mixin;
+
+import dev.gamelord2011.ags_reborn.AntiGrianSwitchReborn;
+
+import net.minecraft.client.render.entity.FallingBlockEntityRenderer;
+import net.minecraft.client.render.entity.state.FallingBlockEntityRenderState;
+import net.minecraft.client.render.state.CameraRenderState;
+import net.minecraft.client.util.math.MatrixStack;
+//import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.command.OrderedRenderCommandQueue;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@SuppressWarnings("null")
+@Mixin(FallingBlockEntityRenderer.class)
+public class Boom {
+	@Inject(
+    method = "render(Lnet/minecraft/client/render/entity/state/FallingBlockEntityRenderState;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/command/OrderedRenderCommandQueue;Lnet/minecraft/client/render/state/CameraRenderState;)V",
+    at = @At("HEAD")
+)
+private void inijectCrash(
+    FallingBlockEntityRenderState fallingBlockEntityRenderState,
+    MatrixStack matrixStack,
+    OrderedRenderCommandQueue orderedRenderCommandQueue,
+    CameraRenderState cameraRenderState,
+    CallbackInfo ci
+	) {
+		if(AntiGrianSwitchReborn.enableFallingEntityBug) {
+			Object renderer = null;
+			renderer.toString();
+		}
+	}
+}
